10f3b45e0f57b50832edee900e030f97e01c33ec
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-04-22 16:53:35 -0500

    used colorgram to extract picture colors

---
+++ assets/images/get_colors.py
@@ -0,0 +1,19 @@
+import colorgram
+
+# colorz = colorgram.extract('color_leaf.jpg', 12)
+
+
+# my_color_list = []
+
+# for color in colorz :
+#     red = color.rgb[0]
+#     green = color.rgb[1]
+#     blue = color.rgb[2]
+#     rgb =  (red, green, blue)
+#     my_color_list.append(rgb)
+
+# print(my_color_list)
+
+# my_6colors = [(70, 16, 9), (13, 33, 48), (142, 23, 13), (17, 29, 24), (180, 49, 24), (61, 14, 18)]
+
+my_12colors = [(70, 16, 9), (13, 33, 48), (142, 23, 13), (17, 29, 24), (180, 49, 24), (61, 14, 18), (37, 88, 130), (131, 20, 24), (239, 65, 32), (30, 62, 106), (107, 64, 72), (66, 80, 72)]
